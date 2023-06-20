432c8ed7e8666ddbb871b944c6d6ea7ea49739d6
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-06-20 16:35:37 +0300

    Structure the webpack, html, js, and css of the site

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+module.exports = {
+
+	mode: 'development',
+		entry: {
+		bundle: path.resolve(__dirname, 'src/index.js'),
+	},
+
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name][contenthash].js',
+		clean: true,
+		assetModuleFilename: '[name][ext]',
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader', 'sass-loader', ],
+			},
+
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					}
+				}
+			},
+
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+		]
+	},
+
+	devtool: 'inline-source-map',
+
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, 'dist'),
+		},
+		port: 3000,
+		open: true,
+		hot: true,
+		compress: true,
+		historyApiFallback: true,
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin ({
+			title: 'ToDo List',
+			filename: 'index.html',
+			template: 'src/index.html',
+		}),
+			new BundleAnalyzerPlugin()
+	]
+}
